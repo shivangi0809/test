9f956c6dd2374fbfc7392472cb3910e7374b56fc
shivangi0809 <72277106+shivangi0809@users.noreply.github.com>
2025-04-11 23:11:27 +0100

    Delete petlab-test/cypress directory

--- petlab-test/cypress/integration/tests/pageLoadTest.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Page Load Test', () => {
-  const urls = require('../../fixtures/urls.json').urls;
-
-  urls.forEach((urlObj) => {
-    it(`should load ${urlObj.url} successfully`, () => {
-      cy.verifyPageLoad(urlObj.url);
-    });
-  });
-});
